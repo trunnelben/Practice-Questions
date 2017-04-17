08f951caa7bf4b6db6b12ec6a92448baa95d5573
Ben Trunnel <trunnelben@wustl.edu>
2017-04-16 23:51:05 -0500

    day21 done

---
+++ 30DaysofCode/day21/generics.java
@@ -0,0 +1,11 @@
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    public void printArray(T[] array){
+        for(T i : array)
+            System.out.println(i);
+    }
+
