c1a946b8586a89198661196be93a47558877052a
Ben Trunnel <trunnelben@wustl.edu>
2017-04-22 14:45:23 -0500

    day 27

---
+++ 30DaysofCode/day27/README.md
@@ -0,0 +1,12 @@
+Objective 
+This challenge is very different from the others we've completed because it requires you to generate a valid test case for a problem instead of solving the problem. There is no input to read, you simply have to generate and print test values for the problem that satisfy both the problem's Input Format and the criteria laid out in the Task section. Check out the Tutorial tab for an instructional video on testing!
+
+Task
+
+Create and print a test case for the problem above that meet the following criteria:
+
+The value of  must be distinct across all the test cases.
+Array  must have at least  zero,  positive integer, and  negative integer.
+Scoring
+
+If you submit  correct test cases, you will earn  of the maximum score. You must submit  test cases to earn the maximum possible score.
---
+++ 30DaysofCode/day27/testing.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println(5);
+        System.out.println(3 + " " + 3);
+        System.out.println(-1 + " " + 0 + " " + 5);
+        System.out.println(4 + " " + 2);
+        System.out.println(0 + " " + -1 + " " + 2 + " " + 1);
+        System.out.println(5 + " " + 3);
+        System.out.println(-7 + " " + 0 + " " + 9 + " " + 8 + " " + 7);
+        System.out.println(6 + " " + 2);
+        System.out.println(0 + " " + -1 + " " + 2 + " " + 1 + " " + 4 + " " + 8);
+        System.out.println(7 + " " + 3);
+        System.out.println(-1 + " " + 0 + " " + 8 + " " + 9 + " " + 8 + " " + 7 + " " + 5);    
+    }
+}
+
