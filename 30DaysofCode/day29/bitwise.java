21bdec0069d45817d97ec91e147aaf46d02ddd74
Ben Trunnel <trunnelben@wustl.edu>
2017-04-26 21:45:15 -0500

    day29 in progress

---
+++ 30DaysofCode/day29/bitwise.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+        }
+    }
+}
+
