bb8251dfb5c9a29fcfa5406acb9f4ff84be73e2f
Ben Trunnel <trunnelben@wustl.edu>
2017-04-28 15:04:40 -0500

    progress on function

--- 30DaysofCode/day29/bitwise.java
+++ 30DaysofCode/day29/bitwise.java
@@ -5,13 +5,17 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-
+    static int maxPermutation(int n, int k){
+        return 5;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for(int a0 = 0; a0 < t; a0++){
             int n = in.nextInt();
             int k = in.nextInt();
+            int answer = maxPermutation(n, k);
+            System.out.println(answer);
         }
     }
 }
